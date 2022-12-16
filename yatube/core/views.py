cfe6fe287fc8ab05e08037eb80ffb38716715f78
Владимир Захаров <zakharov_v_s@hotmail.com>
2022-12-17 05:21:37 +0700

    hw05_6 Commit #3

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -8,5 +8,6 @@ def page_not_found(request, exception):
 def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
 
+
 def error500(request, reason=''):
     return render(request, 'core/500.html')
